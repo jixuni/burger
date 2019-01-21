1e0dcb4ef7622808c75698c5e8fbbdf2b3283b99
Jixu Ni <jixuni@Jixus-MBP.home>
2019-01-21 12:13:41 -0500

    fix connection credential

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 const mysql = require("mysql");
 
-let connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'burgers_db',
-    debug: false
-})
+let connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "jixuf00l",
+      database: "burgers_db"
+    });
+  }
 
 connection.connect((err) => {
     if (err) {
